9f1ff06f299f1c264162ae0ca8c7d2899fe73680
imaptoro <69712952+imaptoro@users.noreply.github.com>
2023-02-18 16:04:20 +1100

    Wall Replacement

--- src/src/config/DesignerConstants.h
+++ src/src/config/DesignerConstants.h
@@ -2,6 +2,6 @@
 
 namespace design
 {
-	constexpr int WALL_SPRITESHEET_COLUMNS = 7;
-	constexpr int WALL_SPRITESHEET_ROWS = 10;
+	constexpr int WALL_SPRITESHEET_COLUMNS = 10;
+	constexpr int WALL_SPRITESHEET_ROWS = 20;
 }
