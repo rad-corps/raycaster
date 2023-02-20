296dffc947618c964d4ba535a8945627f91f84bd
Adam Hulbert <hulbert.adam@gmail.com>
2023-02-20 20:04:52 +1100

    expose player movement and rotation speed to designer header (#50)

--- src/src/config/DesignerConstants.h
+++ src/src/config/DesignerConstants.h
@@ -4,4 +4,7 @@ namespace design
 {
 	constexpr int WALL_SPRITESHEET_COLUMNS = 10;
 	constexpr int WALL_SPRITESHEET_ROWS = 20;
+
+	constexpr float PLAYER_MOVEMENT_SPEED = 1.2f;
+	constexpr float PLAYER_ROTATION_SPEED = 0.04f;
 }
