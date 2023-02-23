f28b1b5c042ea7b235fe8be08803832999d2cc8e
imaptoro <69712952+imaptoro@users.noreply.github.com>
2023-02-23 19:56:31 +1100

    WallTilesAndMaps20230223

--- res/rooms/tileset/walls.tsx
+++ res/rooms/tileset/walls.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="walls" tilewidth="64" tileheight="64" tilecount="70" columns="7">
- <image source="../../img/WallTileset.png" width="448" height="640"/>
+<tileset version="1.9" tiledversion="1.9.2" name="walls" tilewidth="64" tileheight="64" tilecount="200" columns="10">
+ <image source="../../img/WallTileset.png" width="640" height="1280"/>
 </tileset>
